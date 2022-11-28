8b6dd27c8142a8d9925ce8d96e3d08b72631d915
Charoda <Akhmeddzhan@mail.ru>
2022-11-28 03:49:32 +0300

    bootstrap_fetch

---
+++ src/main/resources/static/UserPage.js
@@ -0,0 +1,30 @@
+$(async function() {
+    await thisUser();
+});
+async function thisUser() {
+    fetch("http://localhost:8189/api/users/viewUser")
+        .then(res => res.json())
+        .then(data => {
+            // Добавляем информацию в шапку
+            $('#headerUsername').append(data.email);
+            let roles = data.roles.map(role => " " + role.name.replace("ROLE_", ""));
+            $('#headerRoles').append(roles);
+
+            //Добавляем информацию в таблицу
+            let userRoles = ``
+            for (let role of data.roles) {
+                userRoles += role.name.replace("ROLE_", "") + ' '
+            }
+            let user = `$(
+            <tr>
+                <td>${data.id}</td>
+                <td>${data.username}</td>
+                <td>${data.email}</td>
+                <td>${userRoles}</td>)
+                 `;
+            $('#userPanelBody').append(user);
+        })
+}
+
+
+
